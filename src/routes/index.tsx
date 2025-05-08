3d01b897481cfcf2e6b891f366fdacd3ff653708
goda-kazuki <kazuki.goda@serverworks.co.jp>
2025-05-08 06:52:07 +0000

    shadcn button 追加

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
+import '../index.css'
+import { Button } from "@/components/ui/button"
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -6,8 +8,14 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
+    <div className="p-2 text-3xl">
       <h3>Welcome Home!</h3>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+        <Button variant="destructive">Click me</Button>
+        <Button size="sm">Click me</Button>
+      </div>
     </div>
+    
   )
 }
